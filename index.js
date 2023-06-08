52bee6c8e0bcc7f019a21e32bb61d688972a3c69
shahadat10033 <shahadat10033@gmail.com>
2023-06-08 16:25:10 +0600

    using middleware

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
