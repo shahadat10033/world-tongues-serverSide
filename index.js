7d7e8d2cd4b99c4ec6b77ea558c168cc124ec08d
shahadat10033 <shahadat10033@gmail.com>
2023-06-08 16:23:06 +0600

    install dotenv and require it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,5 +9,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`world tongues server running on port ${port}`);
 });
