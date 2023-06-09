b81bb825a1a363a2c13ecbf9d2938a13610fb35b
shahadat10033 <shahadat10033@gmail.com>
2023-06-09 19:07:31 +0600

    existing user prevent to insert in database

--- index.js
+++ index.js
@@ -51,7 +51,11 @@ async function run() {
 
     app.post("/loggedInUsers", async (req, res) => {
       const user = req.body;
-
+      const query = { email: user.email };
+      const existingUser = await usersDbCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
       const result = await usersDbCollection.insertOne(user);
       res.send(result);
     });
