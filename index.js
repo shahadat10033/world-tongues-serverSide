65088606d11ba3bd8174a0bfba64f50b1fb405b2
shahadat10033 <shahadat10033@gmail.com>
2023-06-12 21:57:01 +0600

    server is ready to deploy in vercel

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
+    useUnifiedTopology: true,
+    maxPoolSize: 10,
   },
+  useNewUrlParser: true,
 });
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
 
     const classesDbCollection = client.db("classDB").collection("classes");
     const selectedClassesDbCollection = client
